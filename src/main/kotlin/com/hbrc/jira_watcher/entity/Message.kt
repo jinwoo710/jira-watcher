63fb535675b7be221ca0bfeffee0e5c4c954030a
jinwoo710 <jinwoo710@naver.com>
2023-01-13 17:39:15 +0900

    message(identity로수정

--- src/main/kotlin/com/hbrc/jira_watcher/entity/Message.kt
+++ src/main/kotlin/com/hbrc/jira_watcher/entity/Message.kt
@@ -8,7 +8,7 @@ import javax.persistence.*
 @Entity
 class Message (
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int = 0,
     var type: String = "",
     @Column(length = 32768)
